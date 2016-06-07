23d5cc4e05f9cf005df799b3af0cd1dcc6f24725
iTamerlanium <macbookair@MacBook-Air-3.local>
2016-06-07 13:50:35 +1200

    Server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var srv = http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type" : "text/plain"});
+  res.write("Hello world!");
+  res.end();
+});
+
+srv.listen(8000);
