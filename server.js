bdbe3943fd6d83f13ac98e001e2d760e2da06b6c
iTamerlanium <macbookair@MacBook-Air-3.local>
2016-06-08 11:32:16 +1200

    Changing bootstrap theme

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var srv = http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type" : "text/plain"});
-  res.write("Hello world!");
-  res.end();
-});
-
-srv.listen(8080);
